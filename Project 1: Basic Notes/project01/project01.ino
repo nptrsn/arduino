2c4986c8e06ed59b65c563a81196747e79a2f5d2
nptrsn <nick@hindiemedia.com>
2015-02-21 12:13:23 -0800

    Project One

---
+++ Project 1: Basic Notes/project01/project01.ino
@@ -0,0 +1,9 @@
+// Transducers: change other types of engergy into electrical energy and vice versa
+  // Sensors: covert other forms of energy into electrical energy
+  // Actuators: covert electrical energy into other types of energy
+ 
+// Current: amount of electrical charge flowing past a specific point. Measured in A (amps)
+// Voltage is the difference in energy between one point in a circuit and another (V)
+// Resistance is how much a compnent resists the flow of electrical energy (Ohm symbol)
+
+// Example: height of a cliff is the Voltage, the rocks tumbling down it is the Current, any brush or trees in the way is Resistance
