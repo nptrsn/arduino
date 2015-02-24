55f4cae20008f3b6b4503c9053c5ea352d1303cf
nptrsn <nick@hindiemedia.com>
2015-02-24 10:58:04 -0800

    Functioning Thermometer w/ serial print of Voltage, Sensor Value, Celcius and Fahrenheit

--- Thermometer/thermometer/thermometer.ino
+++ Thermometer/thermometer/thermometer.ino
@@ -14,5 +14,22 @@ void setup() {
 void loop() {
   int sensorVal = analogRead(sensorPin);
   Serial.print("Sensor Value: ");
-  Serial.print(sensorVal + " ");
+  Serial.print(sensorVal);
+  //convert the sersnor into voltage
+  float voltage = (sensorVal/1024.9) * 5.0;
+  //print the voltage
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  //print the Celcius Temp
+  Serial.print(", Celcius degrees: ");
+  //convert the voltage to Celcius temperature
+  float tempC = (voltage - .5) * 100;
+  Serial.println(tempC);
+  //convert the temp to Fahrenheit degrees
+  float tempF = ((tempC * 9) / 5) + 32;
+  float TempF = (tempC * 9/5) + 32;
+  Serial.print(", Fahrenheit degrees: ");
+  Serial.println(tempF);
+  Serial.print(", second Temp F");
+  Serial.println(TempF);
 }
